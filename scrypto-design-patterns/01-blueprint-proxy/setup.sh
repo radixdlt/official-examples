6aefdf73e7284d685287444e294cf6b0b1002512
Lukasz Rubaszewski <lukasz.rubaszewski@rdx.works>
2024-03-05 13:05:31 +0100

    Cleanup

--- scrypto-design-patterns/01-blueprint-proxy/setup.sh
+++ scrypto-design-patterns/01-blueprint-proxy/setup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 echo -e "\n1. Create a new account and export the account address and owner badge."
-#export account=account_sim1c956qr3kxlgypxwst89j9yf24tjc7zxd4up38x37zr6q4jxdx9rhma
-#export owner_badge=resource_sim1nfzf2h73frult99zd060vfcml5kncq3mxpthusm9lkglvhsr0guahy:#1#
 
-temp_account=`resim new-account`
-echo "$temp_account"
+output=`resim new-account`
+echo "$output"
 
-export account=`echo "$temp_account" | grep Account | grep -o "account_.*"`
-export privatekey=`echo "$temp_account" | grep Private | sed "s/Private key: //"`
+export account=`echo "$output" | grep Account | grep -o "account_.*"`
+export privatekey=`echo "$output" | grep Private | sed "s/Private key: //"`
 
-export owner_badge=`echo "$temp_account" | grep Owner | grep -o "resource_.*"`
+export owner_badge=`echo "$output" | grep Owner | grep -o "resource_.*"`
 export owner_badge_address=`echo "$owner_badge" | cut -f1 -d":"`
 export owner_badge_id=`echo "$owner_badge" | cut -f2 -d":"`
 
@@ -33,9 +31,9 @@ export oracle_manager_badge=${oracle_manager_badge_address}:${oracle_manager_bad
 echo -e "\n3. Mint some tokens and export their addresses."
 
 echo "Mint ETH"
-token=`resim new-token-fixed --name "Ethereum" --symbol "ETH" 1000000`
+output=`resim new-token-fixed --name "Ethereum" --symbol "ETH" 1000000`
 echo "Mint USDT"
-token=`resim new-token-fixed --name "Usdt" --symbol "USDT" 1000000`
+output=`resim new-token-fixed --name "Usdt" --symbol "USDT" 1000000`
 
 export xrd=`resim show $account | grep XRD | grep -o "resource_.*" | cut -f1 -d":"`
 export eth=`resim show $account | grep ETH | grep -o "resource_.*" | cut -f1 -d":"`
