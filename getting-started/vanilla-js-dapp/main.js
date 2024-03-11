467fb67e9748c4c3177dc00a6149c6c27220d79c
Bryan <motosharpley@gmail.com>
2024-03-11 15:09:21 -0700

    add free token transaction

--- getting-started/vanilla-js-dapp/main.js
+++ getting-started/vanilla-js-dapp/main.js
@@ -6,6 +6,7 @@ import helloTokens from './assets/hello-tokens.png'
 import { RadixDappToolkit, DataRequestBuilder, RadixNetwork } from '@radixdlt/radix-dapp-toolkit'
 
 
+
 // Inject the navbar into the DOM
 document.querySelector('#navbar-container').innerHTML = `
     <div id="navbar">
@@ -21,19 +22,52 @@ document.querySelector('#hello-tokens').innerHTML = `<img src="${helloTokens}" a
 
 // You can create a dApp definition in the dev console at https://stokenet-console.radixdlt.com/dapp-metadata 
 // then use that account for your dAppId
-const dAppId = 'account_tdx_2_12yea7979c8e87zwsnx2pu53g67qruemy7ur2vsg8445l3fwgxly78q'
+const dAppId = 'account_tdx_2_128jm6lz94jf9tnec8d0uqp23xfyu7yc2cyrnquda4k0nnm8gghqece'
 // Instantiate DappToolkit
 const rdt = RadixDappToolkit({
   dAppDefinitionAddress: dAppId,
   networkId: RadixNetwork.Stokenet, // network ID 2 is for the stokenet test network, network ID 1 is for mainnet
-  applicationName: 'Hello Scrypto dApp',
+  applicationName: 'Hello Token dApp',
   applicationVersion: '1.0.0',
 })
 console.log("dApp Toolkit: ", rdt)
+
+// Global States
+let accountAddress;
+let componentAddress = 'component_tdx_2_1czajlaar6m2r35sngvekzwlztkvkmynfe35ed4kgxwem2j2ns8a388'
+
 
 // ************ Fetch the user's account address ************
 rdt.walletApi.setRequestData(DataRequestBuilder.accounts().atLeast(1))
 // Subscribe to updates to the user's shared wallet data
 rdt.walletApi.walletData$.subscribe((walletData) => {
   console.log("subscription wallet data: ", walletData)
+  accountAddress = walletData.accounts[0].address
 })
+
+// Send a transaction to the wallet when user clicks on the claim token button Id=get-hello-token
+document.getElementById('get-hello-token').onclick = async function () {
+
+  let manifest = `
+  CALL_METHOD
+    Address("${componentAddress}")
+    "free_token"
+    ;
+  CALL_METHOD
+    Address("${accountAddress}")
+    "deposit_batch"
+    Expression("ENTIRE_WORKTOP")
+    ;
+  `
+  console.log("manifest: ", manifest)
+  // Send manifest to extension for signing
+  const result = await rdt.walletApi
+    .sendTransaction({
+      transactionManifest: manifest,
+      version: 1,
+    })
+  if (result.isErr()) throw result.error;
+  console.log("free token result:", result.value);
+  // let getCommitReceipt = await rdt.gatewayApi.transaction.getCommittedDetails(result.value.transactionIntentHash)
+  // console.log('getCommittedDetails:', getCommitReceipt)
+}
