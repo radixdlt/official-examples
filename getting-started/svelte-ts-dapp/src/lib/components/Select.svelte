1c030016df73603a90dc8383bfe4bbcb8be39b4c
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-04-12 10:05:02 +0100

    add getting-stated svelte ts template

---
+++ getting-started/svelte-ts-dapp/src/lib/components/Select.svelte
@@ -0,0 +1,152 @@
+<script lang="ts">
+  export let label: string;
+  export let options: { value: string; label: string }[] = [];
+
+  let active = false;
+
+  // add a click event to select button
+  const handleClick = () => {
+    active = !active;
+  };
+</script>
+
+<div class="select">
+  <button
+    on:click={handleClick}
+    class="button"
+    role="combobox"
+    aria-labelledby="select button"
+    aria-haspopup="listbox"
+    aria-expanded={active}
+    aria-controls="dropdown">
+    <span class="selected-value">{label}</span>
+    <span class="arrow" style={active ? "transform: rotate(180deg)" : ""} />
+  </button>
+  <ul class="dropdown {active ? 'active' : ''}" role="listbox">
+    {#each options as option}
+      <li
+        role="option"
+        aria-selected="false"
+        on:click={() => {
+          label = option.label;
+        }}
+        on:keypress={(e) => {
+          if (e.key === "Enter") label = option.label;
+        }}>
+        <input type="radio" name="account" id="" value="${option.value}" />
+        <label for="${option.label}">
+          {option.label}
+        </label>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .select {
+    position: relative;
+    width: 400px;
+    max-width: 100%;
+    font-size: 1.15rem;
+    color: #000;
+  }
+
+  .button {
+    width: 100%;
+    font-size: 1.15rem;
+    background-color: var(--grey-2);
+    padding: 0.675em 1em;
+    border: 1px solid var(--grey-5);
+    border-radius: 8px;
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .selected-value {
+    text-align: left;
+    color: var(--grey-6);
+  }
+
+  .arrow {
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-top: 6px solid var(--grey-5);
+    transition: transform ease-in-out 0.15s;
+  }
+
+  .dropdown {
+    position: absolute;
+    list-style: none;
+    width: calc(100% - 1.5rem);
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.2);
+    background-color: var(--grey-2);
+    border: 1px solid var(--grey-5);
+    border-radius: 8px;
+    padding: 0.6em;
+    margin-top: 0.5rem;
+    max-height: 200px;
+    overflow-y: auto;
+    transition: 0.15s ease;
+    transform: scaleY(0);
+    opacity: 0;
+    visibility: hidden;
+  }
+
+  .dropdown:focus-within {
+    box-shadow: 0 10px 25px rgba(94, 108, 233, 0.6);
+  }
+
+  .dropdown li {
+    position: relative;
+    cursor: pointer;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    color: var(--grey-6);
+    border-radius: 8px;
+    background: var(--gradient-account-1);
+  }
+
+  .dropdown li label {
+    width: 100%;
+    padding: 8px 10px;
+    cursor: pointer;
+  }
+
+  .dropdown::-webkit-scrollbar {
+    width: 7px;
+  }
+
+  .dropdown::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 25px;
+  }
+
+  .dropdown::-webkit-scrollbar-thumb {
+    background: #ccc;
+    border-radius: 25px;
+  }
+
+  .dropdown li:hover,
+  .dropdown input:checked ~ label {
+    background-color: #f2f2f2;
+  }
+
+  .dropdown input:focus ~ label {
+    background-color: #dfdfdf;
+  }
+
+  .dropdown input[type="radio"] {
+    position: absolute;
+    left: 0;
+    opacity: 0;
+  }
+
+  .active {
+    opacity: 1;
+    visibility: visible;
+    transform: scaleY(1);
+  }
+</style>
---
+++ getting-started/svelte-ts-dapp/src/routes/+layout.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import {
+    DataRequestBuilder,
+    RadixDappToolkit,
+    RadixNetwork,
+  } from "@radixdlt/radix-dapp-toolkit";
+  import { onMount } from "svelte";
+  import { rdt, walletData } from "$lib/stores";
+  import { dAppId } from "$lib/constants";
+  import Nav from "./Nav.svelte";
+
+  onMount(() => {
+    // Initialize Radix Dapp Toolkit for connect button, wallet and gateway api usage
+    $rdt = RadixDappToolkit({
+      dAppDefinitionAddress: dAppId,
+      networkId: RadixNetwork.Stokenet, // network ID 2 is for the stokenet test network, network ID 1 is for mainnet
+      applicationName: "Hello Token dApp",
+      applicationVersion: "1.0.0",
+    });
+    console.log("dApp Toolkit: ", rdt);
+
+    // Fetch the user's account address(es) from the wallet
+    $rdt?.walletApi.setRequestData(DataRequestBuilder.accounts().atLeast(1));
+
+    // Subscribe to updates to the user's shared wallet data and store it in the walletData store
+    $rdt?.walletApi.walletData$.subscribe((data) => {
+      $walletData = data;
+    });
+  });
+</script>
+
+<Nav />
+
+<main>
+  <slot />
+</main>
+
+<style>
+  :global(:root) {
+    --grey-1: #0d0f16;
+    --grey-2: #12151f;
+    --grey-3: #19203c;
+    --grey-4: #565962;
+    --grey-5: #74777d;
+    --grey-6: #ffffff;
+    --radix-blue: #052cc0;
+    --radix-pink: #ff43ca;
+    --gradient-account-1: linear-gradient(
+      277deg,
+      #01e2a0 -0.6%,
+      #052cc0 102.8%
+    );
+    --gradient-account-2: linear-gradient(
+      276deg,
+      #ff43ca -14.55%,
+      #052cc0 102.71%
+    );
+
+    font-family: "IBM Plex Sans", sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+
+    color-scheme: light dark;
+    color: var(--grey-6);
+    background-color: var(--grey-2);
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+</style>
